85503f2c7573c4dd1deff7d488eb0d2fe30e8505
Andrew Trent <andrewtrentdesign@gmail.com>
2018-09-30 15:36:51 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,16 @@
+#def reverse_each_word(string)
+#  string_arr = string.split(" ")
+#  string_arr_two = []
+#  string_arr.each do |x|
+#    string_arr_two << x.reverse
+#  end
+#  string_arr_two.join(" ")
+#end
+
 def reverse_each_word(string)
   string_arr = string.split(" ")
   string_arr_two = []
-  string_arr.each do |x|
+  string_arr.collect do |x|
     string_arr_two << x.reverse
   end
   string_arr_two.join(" ")
